381bc5b96455e4ee37ad9cd27208f6066e8154c5
mit <mitp@webenix.net>
2022-02-28 19:44:11 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -13,5 +13,9 @@ module.exports = {
     // ssr and displayName are configured by default
     styledComponents: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
-
